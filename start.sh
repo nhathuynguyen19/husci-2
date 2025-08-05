8a27110d51ab0dd60689fb413b1915a7bd20b94b
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2025-08-05 23:20:40 +0700

    t13

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo "🔃 Starting FastAPI and Discord bot..."
 
 # Chạy FastAPI ở nền
-python3 main_api.py &
+#python3 main_api.py &
 
 # Chạy Discord bot ở foreground để giữ container sống
 python3 main_bot.py
